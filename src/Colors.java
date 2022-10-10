4aa8749d62974cbfd01e64f19395288635895ef5
Adivius <adivius@chaos-siegen.de>
2022-10-11 00:59:25 +0200

    Added color consts

---
+++ src/Colors.java
@@ -0,0 +1,15 @@
+import java.awt.Color;
+
+public class Colors {
+    public static Color BLACK = new Color(0, 0, 0);
+    public static Color RED = new Color(180, 20, 20);
+    public static Color LIGHT_BLUE = new Color(0, 190, 255);
+    public static Color DARK_BLUE = new Color(0, 72, 192);
+    public static Color LIGHT_GREEN = new Color(0, 230, 0);
+    public static Color DARK_GREEN = new Color(16, 180, 34);
+    public static Color YELLOW = new Color(255, 236, 20);
+    public static Color ORANGE = new Color(255, 182, 20);
+    public static Color BROWN = new Color(100, 44, 44);
+    public static Color WHITE = new Color(255, 255, 255);
+    public static Color GREY = new Color(150, 150, 150);
+}
