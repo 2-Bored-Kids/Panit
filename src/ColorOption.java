2e80075fe651dd653e286cb5b9d3b84579c9cb2c
Tempetas <tampetus@gmail.com>
2022-10-11 01:08:56 +0200

    Whoops

---
+++ src/ColorOption.java
@@ -0,0 +1,17 @@
+import java.awt.Color;
+import sum.komponenten.Radioknopf;
+
+public class ColorOption extends Radioknopf {
+  Color color;
+
+  public ColorOption(final double pLinks, final double pOben, final double pBreite, final double pHoehe, final String pAufschrift, Color newColor) {
+    super(pLinks, pOben, pBreite, pHoehe, pAufschrift);
+    
+    color = newColor;
+  }
+
+  @Override
+  protected void knopfGeklickt() {
+    Main.pickColor(color);
+  }
+}
